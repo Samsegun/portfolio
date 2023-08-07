9085e4273b4f04b91b2c4e851fc82b85b5e89589
Samsegun <oyebadesamsegun@gmail.com>
2023-08-07 15:44:44 +0100

    working contact form

--- src/routes/Contact.tsx
+++ src/routes/Contact.tsx
@@ -1,36 +1,117 @@
+import { FormEvent, useRef, useState } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import emailjs from "@emailjs/browser";
+import loadIcon from "../assets/loading.svg";
+
 const Contact = () => {
+    const form = useRef<HTMLFormElement>(null);
+    const [formValues, setFormValues] = useState({
+        name: "",
+        email: "",
+        message: "",
+    });
+    const [isSending, setIsSending] = useState(false);
+
+    const sendEmail = (e: FormEvent) => {
+        e.preventDefault();
+
+        setIsSending(true);
+
+        emailjs
+            .sendForm(
+                import.meta.env.VITE_SERVICE_ID,
+                import.meta.env.VITE_TEMPLATE_ID,
+                form.current!,
+                import.meta.env.VITE_PUBLIC_KEY
+            )
+            .then(
+                response => {
+                    toast.success("Mail sent successfully");
+                    setIsSending(false);
+                    console.log(response);
+
+                    setFormValues({ name: "", email: "", message: "" });
+                },
+                err => {
+                    toast.error("Sending Failed!");
+                    setIsSending(false);
+                    console.log("error " + err);
+                }
+            );
+    };
+
     return (
-        <section className='text-white my-[3.75rem] xl:my-24 mx-[5%]'>
-            <h1 className='heading'>Say Hello.</h1>
+        <>
+            <ToastContainer />
+            <section className='text-white my-[3.75rem] xl:my-24 mx-[5%]'>
+                <h1 className='heading'>Say Hello.</h1>
 
-            <p
-                className='md:text-2xl leading-6 md:leading-[30px]
+                <p
+                    className='md:text-2xl leading-6 md:leading-[30px]
              text-[#ffffffcc] font-lexend font-extralight'>
-                Want to work together? Awesome! Send me a quick message.
-            </p>
+                    Want to work together? Awesome! Send me a quick message.
+                </p>
 
-            <form className='mt-14 flex flex-col gap-4 xl:gap-6'>
-                <div className='flex flex-col gap-4 xl:flex-row xl:gap-6'>
-                    <input name='name' type='text' placeholder='Full Name' />
-                    <input
-                        name='email'
-                        type='email'
-                        placeholder='Email Address'
+                <form
+                    ref={form}
+                    onSubmit={sendEmail}
+                    className='mt-14 flex flex-col gap-4 xl:gap-6'>
+                    <div className='flex flex-col gap-4 xl:flex-row xl:gap-6'>
+                        <input
+                            name='name'
+                            type='text'
+                            placeholder='Full Name'
+                            required
+                            value={formValues.name}
+                            onChange={e =>
+                                setFormValues({
+                                    ...formValues,
+                                    name: e.target.value,
+                                })
+                            }
+                        />
+                        <input
+                            name='email'
+                            type='email'
+                            placeholder='Email Address'
+                            required
+                            value={formValues.email}
+                            onChange={e =>
+                                setFormValues({
+                                    ...formValues,
+                                    email: e.target.value,
+                                })
+                            }
+                        />
+                    </div>
+
+                    <textarea
+                        name='message'
+                        placeholder='Please enter your message'
+                        className='h-36'
+                        required
+                        value={formValues.message}
+                        onChange={e =>
+                            setFormValues({
+                                ...formValues,
+                                message: e.target.value,
+                            })
+                        }
                     />
-                </div>
 
-                <textarea
-                    name='message'
-                    placeholder='Please enter your message'
-                    className='h-36'></textarea>
+                    <button
+                        type='submit'
+                        disabled={isSending}
+                        className='p-5 bg-[#6863FD] mt-8 
+                    rounded-md flex justify-center items-center gap-4'>
+                        <span>Get in Touch</span>
 
-                <button
-                    type='button'
-                    className='p-5 bg-[#6863FD] mt-8 rounded-md'>
-                    Get in Touch
-                </button>
-            </form>
-        </section>
+                        {isSending && <img src={loadIcon} alt='loading' />}
+                    </button>
+                </form>
+            </section>
+        </>
     );
 };
 
