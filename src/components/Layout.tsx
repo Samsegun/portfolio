dc8624fe5e95c57dd0b9f53247138d79e90e9869
Samsegun <oyebadesegunsam@gmail.com>
2024-12-19 16:13:11 +0100

    snowfall overlay

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -2,10 +2,13 @@ import { Outlet } from "react-router-dom";
 import FooterLinks from "./FooterLinks";
 import Header from "./header/header";
 import { Analytics } from "@vercel/analytics/react";
+import { SnowOverlay } from "react-snow-overlay";
 
 const Layout = () => {
     return (
         <div className='bg-[#1a1a1a]'>
+            <SnowOverlay maxParticles={50} />
+
             <Header />
 
             <div className='w-11/12 mx-auto lg:w-4/5 max-w-[1920px] min-h-screen'>
