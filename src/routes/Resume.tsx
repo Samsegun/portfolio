410be35bc858551c41b8a0a8dc7d447b6d2e0592
Samsegun <oyebadesegunsam@gmail.com>
2025-02-17 16:34:00 +0100

    ux message for pdf-viewer

--- src/routes/Resume.tsx
+++ src/routes/Resume.tsx
@@ -3,6 +3,18 @@ import PDFViewer from "../components/PdfViewer";
 const Resume = () => {
     return (
         <section className='text-white my-[3.75rem] xl:my-24 mx-[5%]'>
+            <p className='md:hidden bg-[#6863FD] my-4 rounded-lg p-4 font-bold text-lg'>
+                For the best viewing experience, use a desktop or tablet device.
+                You can also{" "}
+                <a
+                    href='/assets/SAMUEL-OYEBADE-CV.pdf'
+                    download='SAMUEL-OYEBADE-CV.pdf'
+                    className='opacity-70 underline'>
+                    download the PDF{" "}
+                </a>
+                to view it offline.
+            </p>
+
             <PDFViewer pdfUrl='/assets/SAMUEL-OYEBADE-CV.pdf' />
         </section>
     );
